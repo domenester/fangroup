125e36fa7875ca637f5eca01ea324c3837f5e9e8
Diogo Domene <ddomene@interaxa.com>
2023-11-03 15:01:00 -0300

    Adding sh changes

--- fangroup-games/jogo-da-memoria.sh
+++ fangroup-games/jogo-da-memoria.sh
@@ -3,3 +3,9 @@ cd C:\\Users\\Note Dell 2\\Desktop\\jogo-da-memoria\\fangroup-main\\fangroup-mai
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=fangroup --format=\"{{.ID}}\"))
 docker build --tag "fangroup" .
 docker run -p 3000:3000 --restart=unless-stopped --detach fangroup
+
+#!/bin/bash
+cd C:\\Users\\Windows\\Documents\\jogo-da-memoria
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=fangroup --format=\"{{.ID}}\"))
+docker build --tag "fangroup" .
+docker run -p 3000:3000 --restart=unless-stopped --detach fangroup
